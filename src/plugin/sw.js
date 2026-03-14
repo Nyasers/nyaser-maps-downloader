6f1d02f1238347319733d2dfcc52c01baec4ca03
Nyaser <61904116+ProjectNyaser@users.noreply.github.com>
2026-03-14 17:21:38 +0800

    perf(sw): 延迟网络请求以优先返回缓存响应
    将网络请求封装为函数并在返回缓存响应后延迟执行，优化用户体验

--- src/plugin/sw.js
+++ src/plugin/sw.js
@@ -36,20 +36,24 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(
       caches.match(request).then((cachedResponse) => {
         // 无论缓存是否存在，都在后台发起网络请求更新缓存
-        const networkFetch = fetch(request).then((networkResponse) => {
-          // 只有当响应有效时才更新缓存
-          if (networkResponse && networkResponse.ok) {
-            // 先克隆响应对象，然后再使用
-            const clonedResponse = networkResponse.clone();
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(request, clonedResponse);
-            });
-          }
-          return networkResponse;
-        });
-        // 如果缓存存在，立即返回缓存的响应
-        // 否则等待网络请求的响应
-        return cachedResponse || networkFetch;
+        const networkFetch = () =>
+          fetch(request).then((networkResponse) => {
+            // 只有当响应有效时才更新缓存
+            if (networkResponse && networkResponse.ok) {
+              // 先克隆响应对象，然后再使用
+              const clonedResponse = networkResponse.clone();
+              caches.open(CACHE_NAME).then((cache) => {
+                cache.put(request, clonedResponse);
+              });
+            }
+            return networkResponse;
+          });
+        if (cachedResponse) {
+          setTimeout(() => networkFetch(), 5000);
+          return cachedResponse;
+        } else {
+          return networkFetch();
+        }
       }),
     );
   }
